19a78b1d9b462cf73edb131d28f5e8a99a29e864
vagosalv <vagosalvgd@gmail.com>
2022-10-18 19:50:16 +0300

    chart data

--- charts.js
+++ charts.js
@@ -1,35 +1,42 @@
-const ctx = document.getElementById('myChart').getContext('2d');
-const myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
-            }
+const labels = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+  ];
+
+  const data = {
+    labels: labels,
+    datasets: [
+        {
+      label: 'Temperature',
+      backgroundColor: 'rgb(255, 99, 132)',
+      borderColor: 'rgb(255, 99, 132)',
+      data: [0, 10, 5, 2, 20, 30, 45,25],
+        },
+        {
+            label: 'Humidity',
+            borderColor: 'rgb(555, 199, 132)',
+            backgroundColor: 'rgb(555, 199, 132)',
+            data: [40, 15, 52, 2, 28, 2, 45,25],
         }
+    
+    ]
+  };
+
+  const config = {
+    type: 'line',
+    data: data,
+    options: {
+        responsive: true,
     }
-});
+  };
+
+  const myChart = new Chart(
+    document.getElementById('myChart'),
+    config
+  );
