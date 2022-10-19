f1eca3f4f307ff1e6d8d937b0e0b208a7371daea
vagosalv <vagosalvgd@gmail.com>
2022-10-19 23:07:27 +0300

    oloklhrwsh xwrismatos dedomenwn

--- charts.js
+++ charts.js
@@ -41,10 +41,36 @@ const myChart = new Chart(
     config
 );
 
-
+/*diavazei to arxeio csv */
 async function getData(){
+    const date = []; //exei thn wra
+    const temperature = []; //thermokrasia
+    const humidity = []; //exei thn ugrasia
+
     const url = 'DT02-2022-10-17_20_00.csv';
     const responce = await fetch(url);
     const tabledata = await responce.text();
-    console.log(tabledata);
-}
+    //console.log(tabledata);
+
+    const table = tabledata.split('\n');//xwrizoume ta dedomena
+    //console.log(table);
+    table.forEach(row=> {
+        const column = row.split(','); //spame ta dedomena se grammes
+        //console.log(column);
+        const time = column[0]; //apomononoume thn wra
+        const temp = column[1]; //apomononoume thn thermokrasia
+        const humid = column[2]; // apomononoume thn ugrasia
+        //console.log(column[0]);
+
+        //vazoume tis times stis metavlites
+        date.push(time);
+        temperature.push(temp);
+        humidity.push(humid);
+    });
+
+    //elegxos an pernane oi times
+    console.log(date);
+    console.log(temperature);
+    console.log(humidity);
+    return {date, temperature, humidity}
+};
