78f251f2f89008dabb0f04119ab4af5d03c659de
vagosalv <vagosalvgd@gmail.com>
2022-12-20 20:02:09 +0200

    add radius to heatmap

--- scripts/index.js
+++ scripts/index.js
@@ -32,12 +32,13 @@ function initMap() {
         map: map,
     });
 
+    heatmap.set("radius", heatmap.get("radius") ? null : 50);
     //gia heatmap
     function getPoints(){
         return[
-            {location: new google.maps.LatLng(39.624048,19.897758), weight: 0.5},
+            {location: new google.maps.LatLng(39.624048,19.897758), weight: 5},
             {location: new google.maps.LatLng(39.624048,19.898758),  weight: 2},
-            new google.maps.LatLng(39.624048,19.898458),
+            new google.maps.LatLng(39.624048,19.899458),
             new google.maps.LatLng(39.624048,19.898487),  
         ];
     }
